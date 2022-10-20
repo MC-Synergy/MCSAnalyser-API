910697c9bae857f2d79313257d3fb9d11dfa14e6
Vermeire,Vincent V.L <v.vermeire@student.fontys.nl>
2022-10-20 14:57:05 +0200

    added filter for time

--- src/main/java/com/mcs/analyser/production/ProductionRepository.java
+++ src/main/java/com/mcs/analyser/production/ProductionRepository.java
@@ -3,9 +3,11 @@ package com.mcs.analyser.production;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ProductionRepository extends JpaRepository<ProductionDataPoint, Long> {
     List<ProductionDataPoint> findByMcsSystemID(Integer mcsSystemID);
+    List<ProductionDataPoint> findByMcsSystemIDAndTimeSentGreaterThanEqual(Integer mcsSystemID, LocalDateTime date);
 }
